eff4aa42162f37150f60ffec183572970fe003c8
sujin3646 <0502parksujin@gmail.com>
2026-07-24 19:37:23 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,19 +1,39 @@
-document
-.getElementById("searchInput")
-.addEventListener("input", function(){
+const searchInput = document.getElementById("searchInput");
 
-    let keyword = this.value.toLowerCase();
 
-    let result = equipments.filter(function(item){
+searchInput.addEventListener("input", function(){
+
+    const keyword = this.value.toLowerCase();
+
+
+    const result = equipments.filter(item => {
 
         return (
-            item.name.toLowerCase().includes(keyword) ||
-            item.category.toLowerCase().includes(keyword) ||
+
+            item.name.toLowerCase().includes(keyword)
+
+            ||
+
+            item.category.toLowerCase().includes(keyword)
+
+            ||
+
             item.manager.toLowerCase().includes(keyword)
+
+            ||
+
+            item.step.toLowerCase().includes(keyword)
+
+            ||
+
+            item.warning.toLowerCase().includes(keyword)
+
         );
 
     });
 
+
     displayEquipment(result);
+
 
 });
